68fbb8a1a47b2760d0ed210b0702bed0da2f2919
Chemamac <chemamac@Chemamacs-Mac-mini.local>
2015-02-15 23:51:42 -0600

    Initial Commit

---
+++ Lab06Push/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Lab06Push
+//
+//  Created by Chemamac on 2/15/15.
+//  Copyright (c) 2015 Chemamac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Lab06PushTests/Lab06PushTests.m
@@ -0,0 +1,40 @@
+//
+//  Lab06PushTests.m
+//  Lab06PushTests
+//
+//  Created by Chemamac on 2/15/15.
+//  Copyright (c) 2015 Chemamac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface Lab06PushTests : XCTestCase
+
+@end
+
+@implementation Lab06PushTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
